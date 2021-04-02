750317ef53deb14c4906c92f830a444e9fbb8fae
Jacob Piland <piland.jacob@gmail.com>
2021-04-01 20:02:35 -0500

    Add model animal

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,6 +1,12 @@
-car ={
+car = {
   "year" => "2013"
   "make" => "Subaru"
   "model" => "Outback"
   "color" => "white"
+}
+
+animal = {
+  "Legs" => "Four"
+  "Fur" => "Fluffy"
+  "Tail" => "Yes"
 }
